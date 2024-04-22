a2f23b53963945b2ca17a638d875fc6a4bd4764b
Kyle Yuen <103063168+RandomHouseGuest@users.noreply.github.com>
2024-04-22 10:07:20 -0700

    APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,18 @@
 public class APLine
 {
-  /* your code here */
-  
+  private double a, b, c;
+  public APLine(double vala, double valb, double valc){
+    a = vala;
+    b = valb;
+    c = valc;
+  }
+  public double getSlope(){
+    return -a/b;
+  }
+  public boolean isOnLine(double x, double y){
+    if((a*x + b*y + c) == 0){
+      return true;
+    }
+    return false;
+  }
 }
